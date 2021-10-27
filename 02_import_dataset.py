9ce516270bc4ebb2240ab7ec775c9752de049648
Vic Ding <qiqing.ding@dans.knaw.nl>
2021-10-27 16:06:23 +0200

    add variable csv files and upload to corresponding datasets

--- 02_import_dataset.py
+++ 02_import_dataset.py
@@ -1,15 +1,12 @@
 import csv
 import os
-from os.path import join
 
-import lxml.html
 from pyDataverse.exceptions import ApiAuthorizationError
 from pyDataverse.api import Api
-from requests import post
+from requests import put
 import json
 import dvconfig
 import lxml.etree as et
-import lxml.objectify as objectify
 
 base_url = dvconfig.base_url
 native_api_base_url = f'{base_url}/api'
@@ -18,7 +15,15 @@ dataverse_id = dvconfig.dataverse_name
 input_path = dvconfig.cbs_data_path
 cbs_mapping_file = dvconfig.cbs_mapping_file
 
-release = 'no'
+extra_fields_parent: str = "/Dataontwerpversies/Versie/Dataontwerp/Contextvariabelen/Contextvariabele"
+extra_fields = [
+    "VerkorteSchrijfwijzeNaamVariabele",
+    "LabelVanDeVariabele",
+    "Variabele/Definitie",
+    "ToelichtingBijDeDefinitie",
+    "ToelichtingBijHetGebruik",
+    "Datatype"
+]
 
 ns = {
     'xmlns': 'http://www.cbs.nl/Dsc/4.1'
@@ -31,6 +36,7 @@ print('API status: ' + api.status)
 
 
 def get_primitive_field(list_elements, typeName, typeClass='primitive', multiple=False):
+    """get primitive field"""
     result = dict()
     result['typeName'] = typeName
     result['multiple'] = multiple
@@ -53,6 +59,7 @@ def get_primitive_field(list_elements, typeName, typeClass='primitive', multiple
 
 
 def get_compound_field(pf, subfields, dom, type_class='compound'):
+    """get compound filed which consists of parent field and its sub-fields"""
     value_dict = dict()
     static_values = dict()
     result = dict()
@@ -68,7 +75,7 @@ def get_compound_field(pf, subfields, dom, type_class='compound'):
                 # structure of the value list [value, parent field, pf multiple, this multiple]
                 value_dict[row[4]] = [row[0], pf, row[3], row[5]]
             else:
-                raise Exception('cannot have more than one subfields which have xpath.')
+                raise Exception(value_dict.keys(), 'cannot have more than one subfields which have xpath.')
         else:
             static_values[row[4]] = [row[0], pf, row[3], row[5]]
 
@@ -107,7 +114,6 @@ def get_compound_field(pf, subfields, dom, type_class='compound'):
                     inner_values_list.append(inner_dict)
 
         if len(inner_values_list) > 0:
-            # TODO
             if result['multiple']:
                 result['value'] = inner_values_list
             else:
@@ -120,37 +126,31 @@ def get_compound_field(pf, subfields, dom, type_class='compound'):
 
 
 def get_boolean_value(text_value):
+    """get booleean value from a set of text values"""
     positive_boolean_values = ['yes', 'true', '1', 1]
     return True if text_value.lower() in positive_boolean_values else False
 
 
 def get_all_rows_for_current_type(current_type, mapping_csv, key=2):
+    """
+    get all the rows of certain type, either primitive or compound, base on mapping info
+    key indicates where the type info is
+    """
     return [r for r in mapping_csv if r[key] == current_type]
 
 
 def convert_xml_to_dv_json(dom, mapping_csv):
-    processed_fields = list()
+    """convert the give xml dom to Dataverse JSON"""
     result = {
         'datasetVersion': {
             "termsOfUse": "N/A",
             'license': 'NONE',
             'metadataBlocks': {
-                # 'citation': {
-                #     # 'displayName': 'Citation Metadata',
-                #     'fields': [
-                #
-                #     ]
-                # },
-                # 'socialscience': {
-                #     # 'displayName': 'Social Sciences',
-                #     'fields': [
-                #
-                #     ]
-                # }
+
             }
         }
     }
-    # get metadata blocks from mapping
+    """get metadata blocks from mapping"""
     blocks = get_column_names(mapping_csv, 1)
     for b in blocks:
         print(f'running block: {b}')
@@ -161,8 +161,7 @@ def convert_xml_to_dv_json(dom, mapping_csv):
         print(f'all the simple fields of {b}: {current_simple_fields}')
         print(f'all the complex fields of {b}: {current_complex_fields}')
 
-        # preparing all the complex fields for the current block
-        # get all parent fields of current block
+        """prepare all the complex fields for the current block"""
         unique_parent_fields_per_block = get_column_names(current_complex_fields, 2)
         print(f'all the unique parent fields {unique_parent_fields_per_block}')
         for pf in unique_parent_fields_per_block:
@@ -172,7 +171,7 @@ def convert_xml_to_dv_json(dom, mapping_csv):
             if current_complex_field_value is not None:
                 fields.append(current_complex_field_value)
 
-        # preparing all the simple fields for the current block
+        """prepare all the simple fields for the current block"""
         for current_field in current_simple_fields:
             value = current_field[0] if not current_field[0].startswith('/') else dom.xpath(current_field[0])
             # got xml elements using xpath as list
@@ -181,12 +180,14 @@ def convert_xml_to_dv_json(dom, mapping_csv):
                 # reading all the Element and set multiple to True
                 if value is not None and len(value) > 0:
                     print(f'adding simple value: {value} to {current_field[4]}')
-                    current_simple_field_value = get_primitive_field(value, current_field[4], current_field[6], get_boolean_value(current_field[5]))
+                    current_simple_field_value = get_primitive_field(value, current_field[4], current_field[6],
+                                                                     get_boolean_value(current_field[5]))
                     if current_simple_field_value is not None:
                         fields.append(current_simple_field_value)
             else:
                 # got static string as value
-                fields.append(get_primitive_field([value], current_field[4], current_field[6], get_boolean_value(current_field[5])))
+                fields.append(get_primitive_field([value], current_field[4], current_field[6],
+                                                  get_boolean_value(current_field[5])))
 
         # adding all the fields to dict and ready to be dumped to json
         result['datasetVersion']['metadataBlocks'][b]['fields'] = fields
@@ -229,8 +230,83 @@ def get_simple_fields_per_block(fname, mapping_list):
     return [r for r in mapping_list if r[1] == fname and r[2] == '']
 
 
-def __main__():
-    # testing with single json file
+def put_request_semantic(query_str, metadata=None, auth=False, params=None):
+    """Make a PUT request upon semantic API.
+
+    Parameters
+    ----------
+    query_str : string
+        Query string for the request. Will be concatenated to
+        `native_api_base_url`.
+    metadata : string
+        Metadata as a json-formatted string. Defaults to `None`.
+    auth : bool
+        Should an api token be sent in the request. Defaults to `False`.
+    params : dict
+        Dictionary of parameters to be passed with the request.
+        Defaults to `None`.
+
+    Returns
+    -------
+    requests.Response
+        Response object of requests library.
+
+    """
+
+    url: str = f'{native_api_base_url}{query_str}'
+    exit(url)
+
+    if auth:
+        if api_token:
+            if not params:
+                params = {}
+            params['key'] = api_token
+        else:
+            ApiAuthorizationError(
+                'ERROR: PUT - Api token not passed to '
+                '`put_request` {}.'.format(url)
+            )
+
+    try:
+        resp = put(
+            url,
+            data=open(metadata, mode='rb').read(),
+            params=params
+        )
+        if resp.status_code < 200 or resp.status_code > 299:
+            raise Exception('calling semantic API failed')
+        return resp
+    except ConnectionError:
+        raise ConnectionError(
+            'ERROR: PUT - Could not establish connection to api {}.'
+            ''.format(url)
+        )
+
+
+def get_extra_fields(dom) -> list:
+    result: list = []
+    all_elements = dom.xpath(extra_fields_parent)
+    for row in all_elements:
+        for ef in extra_fields:
+            print(ef)
+            elements = row.xpath(ef)
+            for e in elements:
+                result.append([e.tag, e.text])
+        result.append(["", ""])
+    return result
+
+
+def create_temp_csv(extra_field_value, cwd, filename_padding: str = 'VariablesMetadata', postfix='csv') -> str:
+    """create a CSV file with the given content in a temp location, returns filename as str"""
+    full_output_filename = os.path.join(cwd, f'{filename_padding}.{postfix}')
+    with open(full_output_filename, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=",", quoting=csv.QUOTE_ALL)
+        csv_writer.writerows(extra_field_value)
+        return full_output_filename
+
+
+def main() -> None:
+    """testing with single json file"""
     # full_input_file = '/Users/vic/Library/Application Support/JetBrains/PyCharm2021.2/scratches/scratch_5.json'
     # with open(full_input_file, 'r') as jsonfile:
     #     jsonfile = json.load(jsonfile)
@@ -240,30 +316,51 @@ def __main__():
     #     print('okokok')
     # exit()
 
+    cwd = os.getcwd()
     counter = 1
     mapping_list = load_mapping_file(cbs_mapping_file)
     print('mapping file loaded')
     for root, dirs, files in os.walk(input_path):
         for name in files:
-            full_input_file = join(root, name)
+            full_input_file = os.path.join(root, name)
             if full_input_file.endswith('dsc'):
                 print(f'{counter} working on {full_input_file}')
                 counter += 1
                 dom = et.parse(full_input_file)
-                # remove namespaces and use etree in order to use absolute path
+                """remove namespaces and use etree in order to use absolute path"""
                 dom = remove_ns_from_xml(dom)
-                # convert xml file to DV json file
+                """convert xml file to DV json file"""
                 cbs_json = convert_xml_to_dv_json(dom, mapping_list)
                 # print(cbs_json)
                 # exit()
+                """create dataset using the given json"""
                 resp = api.create_dataset(dataverse_id, cbs_json)
-                print(resp.status_code)
                 if not 300 > resp.status_code > 199:
-                    print(resp.text)
-                    print(cbs_json)
-                    raise Exception('error occurred')
+                    raise Exception('error occurred while creating dataset', resp.text, cbs_json)
+                # get pid and ds_id
+                pid = resp.json()["data"]["persistentId"]
+                pid = pid.split('/')[1]
+                ds_id = resp.json()["data"]["id"]
+
+                # TODO: update publication date with semantic API
+                """TODO"""
+                # query_str = f'/datasets/{ds_id}/metadata?replace=true'
+                # semantic_str: str = ''
+                # put_request_semantic(query_str, semantic_str, True)
+
+                # TODO: upload file of variables
+                """get data from xml according to the given xpath list: extra_field"""
+                extra_field_value: list = get_extra_fields(dom)
+                """create upload file and get the filename"""
+                csv_filename: str = create_temp_csv(extra_field_value, cwd)
+                """upload the file using API"""
+                padded_pid = f'uuid:10.5072/{pid}'
+                resp = api.upload_file(padded_pid, csv_filename)
+
+                if resp['status'] != 'OK':
+                    raise Exception('error occurred while uploading datafile to dataset', resp)
 
 
 if __name__ == '__main__':
     print('starting...')
-    __main__()
+    main()
